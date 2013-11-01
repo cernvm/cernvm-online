34e7587fc918a16a1d22f83068ca775f57526a33
George Lestaris <glestaris@gmail.com>
2013-11-01 11:57:45 +0100

    Removing check for user e-mail address from user registration

--- src/cvmo/context/views/user.py
+++ src/cvmo/context/views/user.py
@@ -9,7 +9,7 @@ from django.contrib import auth
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.template import loader
-from django.core import urlresolvers 
+from django.core import urlresolvers
 from Crypto.Random import random
 from Crypto.Hash import SHA256
 from cvmo.context.models import UserActivationKey
@@ -18,7 +18,7 @@ from cvmo.context.utils.views import msg_error, msg_warning, msg_confirm, redire
 def login(request):
     if user_is_logged(request):
         return redirect("dashboard")
-        
+
     # Push session messages to the context
     context = {}
     push_to_context("redirect_msg_info", "msg_info", context, request)
@@ -26,20 +26,20 @@ def login(request):
     push_to_context("login_error", "msg_error", context, request)
     push_to_context("redirect_msg_warning", "msg_warning", context, request)
     push_to_context("redirect_msg_confirm", "msg_confirm", context, request)
-        
+
     return render_to_response("pages/login.html", context, RequestContext(request))
 
 def login_action(request):
     if user_is_logged(request):
         return redirect("dashboard")
-    
+
     # Parse request
     username = request.POST.get('username', '')
     password = request.POST.get('password', '')
     if username == '' or password == '':
         request.session["login_error"] = "Please fill in username and password!"
         return redirect("login")
-    
+
     # Try login user
     user = auth.authenticate(username=username, password=password)
     if user is not None and user.is_authenticated():
@@ -47,7 +47,7 @@ def login_action(request):
         if not user.is_active:
             request.session["login_error"] = "You should verify your email address to activate your account"
             return redirect("login")
-        
+
         auth.login(request, user)
         return redirect("dashboard")
     else:
@@ -58,28 +58,28 @@ def login_action(request):
 def register(request):
     if user_is_logged(request):
         return redirect("dashboard")
-    
+
     # Get form error
     if "form_error" in request.session:
         context = { "msg_error": request.session["form_error"] }
         del request.session["form_error"]
     else:
         context = {}
-    
+
     # Reproduce form
     if "form" in request.session:
         context["user"] = request.session["form"]
         del request.session["form"]
-        
+
     # Add Google Recaptcha Key
     context["recaptcha_public_key"] = settings.GOOGLE_RECAPTCHA["public_key"]
-    
+
     return render_to_response("pages/register.html", context, RequestContext(request))
 
 def register_action(request):
     if user_is_logged(request):
-        return redirect("dashboard")    
-    
+        return redirect("dashboard")
+
     # Parse request
     username = request.POST.get('username', '')
     password = request.POST.get('password', '')
@@ -92,32 +92,32 @@ def register_action(request):
         "email": email,
         "first_name": firstName,
         "last_name": lastName
-    }    
-    
+    }
+
     # Required fields filled?
     if username == '' or password == '' or retypePassword == '' or email == '':
         request.session["form_error"] = "Please fill in all required fields!"
         request.session["form"] = form
         return redirect("register")
-    
+
     # Check if recaptcha is valid
     if not verify_recaptcha(request):
         request.session["form_error"] = "Please fill carefully the CAPTCHA field!"
         request.session["form"] = form
         return redirect("/register")
-    
+
     # Check if email is valid
     if not is_valid_email(email):
         request.session["form_error"] = "Please provide a valid email address"
         request.session["form"] = form
         return redirect("register")
-    
+
     # Are password the same?
     if password != retypePassword:
         request.session["form_error"] = "Passwords are not the same"
         request.session["form"] = form
         return redirect("register")
-    
+
     # Is username used?
     try:
         user = User.objects.get(username=username)
@@ -126,16 +126,16 @@ def register_action(request):
         return redirect("register")
     except:
         pass
-      
+
     # Is email used?
-    try:
-        user = User.objects.get(email=email)
-        request.session["form_error"] = "User with email address %s already exists." % email
-        request.session["form"] = form
-        return redirect("register")
-    except:
-        pass    
-    
+    # try:
+    #     user = User.objects.get(email=email)
+    #     request.session["form_error"] = "User with email address %s already exists." % email
+    #     request.session["form"] = form
+    #     return redirect("register")
+    # except:
+    #     pass
+
     # Create user
     user = User(username=username,email=email)
     user.set_password(password)
@@ -148,11 +148,11 @@ def register_action(request):
         request.session["form_error"] = "Failed to store new user!"
         request.session["form"] = form
         return redirect("register")
-    
+
     # Send activation email
     send_activation_email(request,user)
-    
-    # Redirect to login    
+
+    # Redirect to login
     request.session["redirect_msg_info"] = "Your account has been created. \
     Please check you inbox in %s to activate your account." % user.email
     return redirect("login")
@@ -168,40 +168,40 @@ def logout(request):
 
     # Logout from Django (Important)
     auth.logout(request)
-    
-    # Check if the user if from cern 
+
+    # Check if the user if from cern
     if sso_user:
         return redirect("https://login.cern.ch/adfs/ls/?wa=wsignout1.0")
-    
+
     # Otherwise, go back to login
     return redirect("login")
 
 def profile_edit(request):
     if not user_is_logged(request):
         return redirect("login")
-    
+
     # Get form error
-    if "form_error" in request.session:        
+    if "form_error" in request.session:
         context = { "msg_error": request.session["form_error"] }
         del request.session["form_error"]
     else:
         context = {}
-    
+
     # Reproduce form
     if "form" in request.session:
         context["user"] = request.session["form"]
         del request.session["form"]
     else:
         context["user"] = request.user
-        
+
     context["profile_edit"] = True
-    
+
     return render_to_response("pages/profile.html", context, RequestContext(request))
 
 def profile_edit_action(request):
     if not user_is_logged(request):
         return redirect("login")
-    
+
     # Parse request
     currentPassword = request.POST.get("current_password", "")
     newPassword = request.POST.get('password', '')
@@ -213,20 +213,20 @@ def profile_edit_action(request):
         "email": email,
         "first_name": firstName,
         "last_name": lastName
-    }    
-    
+    }
+
     # Required fields filled?
     if email == '':
         request.session["form_error"] = "Please fill in all required fields!"
         request.session["form"] = form
-        return redirect("profile")     
-    
+        return redirect("profile")
+
     # Check if email is valid
     if not is_valid_email(email):
         request.session["form_error"] = "Please provide a valid email address"
         request.session["form"] = form
         return redirect("profile")
-        
+
     # Change password
     if currentPassword != '':
         # Check old password
@@ -234,31 +234,31 @@ def profile_edit_action(request):
             request.session["form_error"] = "Old password is not corrent"
             request.session["form"] = form
             return redirect("profile")
-        
+
         # Are new passwords the same?
         if newPassword != retypeNewPassword:
             request.session["form_error"] = "Passwords are not the same"
             request.session["form"] = form
             return redirect("profile")
-            
+
     # Is email used?
     try:
         user = User.objects.get(email=email)
         # Ignore same user i.e. if email is unchanged
-        if user.username != request.user.username:            
+        if user.username != request.user.username:
             request.session["form_error"] = "User with email address %s already exists." % email
             request.session["form"] = form
             return redirect("profile")
     except:
-        pass    
-    
+        pass
+
     # Modify user
     request.user.first_name = firstName
     request.user.last_name = lastName
     request.user.email = email
     if newPassword != '':
         request.user.set_password(newPassword)
-        
+
     # Store changes
     try:
         request.user.save()
@@ -266,34 +266,34 @@ def profile_edit_action(request):
         request.session["form_error"] = "Failed to store new user!"
         request.session["form"] = form
         return redirect("profile")
-    
+
     request.session["redirect_msg_info"] = "Your account has been modified!"
     return redirect("dashboard")
 
 def account_activation(request):
     # Parse request
     key = request.GET.get( "key", "" )
-    
+
     # Find UserActivationKey
     try:
         activationKey = UserActivationKey.objects.get(key=key)
-        
+
         # Update user
         activationKey.user.is_active = 1
         activationKey.user.save()
-        
+
         # Delete the key
         activationKey.delete()
-        
-        request.session["redirect_msg_info"] = "User %s has been activated. You can now login." % activationKey.user.username        
+
+        request.session["redirect_msg_info"] = "User %s has been activated. You can now login." % activationKey.user.username
     except:
         # Key not found
         request.session["redirect_msg_error"] = "Activation key is invalid!"
-    
-    return redirect("login")                
+
+    return redirect("login")
 
 """
-   Helper functions 
+   Helper functions
 """
 
 def send_activation_email(request, user):
@@ -305,41 +305,41 @@ def send_activation_email(request, user):
         key = h.hexdigest()
     )
     userActivationKey.save()
-    
+
     # Create activation link
     if request.is_secure():
         activationLink = "https://"
-    else: 
+    else:
         activationLink = "http://"
     activationLink += request.get_host()
     activationLink += urlresolvers.reverse("account_activation")
     activationLink += "?key=" + userActivationKey.key
-    
+
     # Get email template
     context = {
         "user": user,
-        "activate_link": activationLink 
+        "activate_link": activationLink
     }
     emailContent = loader.render_to_string("verification_email.txt", context)
-    
+
     # Prepare the mail
     sender = settings.ACTIVATION_EMAIL["sender_email"]
     receivers = [user.email]
     if user.first_name != '' or user.last_name != '':
         userDisplayName = user.first_name + " " + user.last_name
     else:
-        userDisplayName = user.username        
+        userDisplayName = user.username
     message = """From: """ + settings.ACTIVATION_EMAIL["sender"] + """ <""" + settings.ACTIVATION_EMAIL["sender_email"] + """>
 To: """ + userDisplayName + """ <""" + user.email + """>
 Subject: """ + settings.ACTIVATION_EMAIL["subject"] + """
 """
-    message += "\n" + emailContent + "\n"    
+    message += "\n" + emailContent + "\n"
 
     # Send the email
     try:
         smtpObj = smtplib.SMTP('localhost')
         smtpObj.sendmail(sender, receivers, message)
-        return True         
+        return True
     except:
         if settings.DEBUG:
             print "===================================================================================="
@@ -347,7 +347,7 @@ Subject: """ + settings.ACTIVATION_EMAIL["subject"] + """
             print emailContent
             print "===================================================================================="
 
-        return False    
+        return False
 
 def user_is_logged(request):
     return request.user is not None \
@@ -368,12 +368,12 @@ def verify_recaptcha(request):
              ("challenge", request.POST.get("recaptcha_challenge_field", "")),
              ("response", request.POST.get("recaptcha_response_field", ""))
     ]
-    data = urllib.urlencode(query) 
-    
+    data = urllib.urlencode(query)
+
     # Create the handle
     try:
         handle = urllib2.urlopen(baseURL, data)
-        
+
         # Get response line
         lines = []
         line = handle.readline()
@@ -381,8 +381,8 @@ def verify_recaptcha(request):
             lines.append(line)
 #            if settings.DEBUG:
 #                print line
-            line = handle.readline()                    
-        
+            line = handle.readline()
+
         # Check first line - it may contain some whitespace characters
         if re.match("^\s*true\s*$", lines[0]) != None:
             return True
@@ -391,7 +391,7 @@ def verify_recaptcha(request):
     except:
         # URLError occurred
         return False
-    
+
 def push_to_context(sessionName,contextName,context,request):
     if sessionName in request.session:
         context[contextName] = request.session[sessionName]
@@ -407,11 +407,11 @@ def bulk_add(request):
     if not request.user.is_staff:
         msg_error(request, "You do not have permission to access this resource!")
         return redirect("dashboard")
-    
+
     # Display bulk add screen
     mem_data = get_memory(request,'data', '')
     return render_to_response("pages/users_bulkadd.html", { "data": mem_data }, RequestContext(request))
-    
+
 def bulk_add_commit(request):
     """
     Commit the bulky-added users
@@ -422,7 +422,7 @@ def bulk_add_commit(request):
     if not request.user.is_staff:
         msg_error(request, "You do not have permission to access this resource!")
         return redirect("dashboard")
-    
+
     # Parse the users
     data = request.POST.get('data', '')
     users = re.split(r'\r?\n', data)
@@ -430,22 +430,22 @@ def bulk_add_commit(request):
     for u in users:
         u = u.strip()
         line += 1
-        
+
         # Skip empty lines
         if (u == ''):
             continue
-        
+
         # Process line
         data = u.split(",")
         if len(data) < 2:
             msg_warning(request, "Expected <strong>user,password, ...</strong> syntax on line "+str(line))
             return redirect_memory('bulk_add', request)
-        
+
         # Add user
         try:
             u = User(username=data[0])
             u.set_password(data[1])
-        
+
             # Setup optional fields
             if len(data) >= 3:
                 u.first_name = data[2]
@@ -453,7 +453,7 @@ def bulk_add_commit(request):
                 u.last_name = data[3]
             if len(data) >= 5:
                 u.email = data[4]
-        
+
             # Add user
             u.save()
             msg_confirm(request, 'User '+data[0]+' added!')
@@ -461,7 +461,7 @@ def bulk_add_commit(request):
         except Exception as ex:
             msg_error(request, "Error while adding user "+data[0]+": "+str(ex))
             return redirect_memory('bulk_add', request)
-                
+
     # Go to dashboard
     return redirect("dashboard")
-    
+
