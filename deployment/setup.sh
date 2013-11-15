3ef085acf823661c6e21cd50db42474fd3bf821c
Dario Berzano <dario.berzano@cern.ch>
2013-11-15 17:05:01 +0100

    Speed up and harmonization of deployment script
    A single script (setup.sh) is now used both for first installation
    and for updating.

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -5,9 +5,10 @@
 #
 
 GIT_REPO="https://github.com/cernvm/cernvm-online.git"
-GIT_BRANCH="master"
+#GIT_BRANCH="master"
+GIT_BRANCH="<dont_change_branch>"
 BASE_DIR="/var/www/cernvm-online"
-SCRIPT_PATH=$(cd $(dirname $BASH_SOURCE) && pwd)
+SCRIPT_PATH=$(cd `dirname "$BASH_SOURCE"` && pwd)
 LOG_FILE="$SCRIPT_PATH/log.txt"
 rm $LOG_FILE 2> /dev/null # clean files
 
@@ -15,8 +16,20 @@ rm $LOG_FILE 2> /dev/null # clean files
 source $SCRIPT_PATH/utils.sh
 
 # Steps
-declare -a STEPS=("make_dirs export_source prepare_python_env install_mysql install_cmvo configure_cvmo make_public install_apache")
+declare -a STEPS=( make_dirs export_source \
+                   prepare_python_env install_mysql \
+                   install_cvmo configure_cvmo \
+                   make_public install_apache \
+                   run_apache )
+declare -a QUICK_STEPS=( install_cvmo configure_cvmo run_apache )
 declare -a STEPS_BACKTRACE=()
+
+# Parse command-line arguments
+for (( I=0 ; I<=$# ; I++ )) ; do
+  if [ "${!I}" == '-q' ] || [ "${!I}" == '--quick' ] ; then
+    STEPS=( "${QUICK_STEPS[@]}" )
+  fi
+done
 
 # Load setup handlers
 source $SCRIPT_PATH/setup_handlers.sh
--- deployment/utils.sh
+++ deployment/utils.sh
@@ -9,8 +9,13 @@ source /etc/init.d/functions
 
 function managed_exec
 {
-    eval "$@" >>$LOG_FILE 2>&1
-    return $?
+    export LAST_LOG_FILE=$(mktemp)
+    local RETVAL
+    echo ">>> BEGIN OF OPERATION: $@" >> "$LOG_FILE"
+    "$@" 2>&1 | tee $LAST_LOG_FILE >> $LOG_FILE
+    RETVAL=${PIPESTATUS[0]}
+    echo "<<< END($RETVAL) OF OPERATION: $@" >> "$LOG_FILE"
+    return $RETVAL
 }
 
 function execute_step
@@ -29,33 +34,60 @@ function undo_step
     return $?
 }
 
+function yuminst
+{
+    local P INSTALL
+    INSTALL=''
+    for P in $@ ; do
+      rpm -qa | grep "^${P}-" || INSTALL="$INSTALL $P"
+    done
+    if [ "$INSTALL" != '' ] ; then
+      yum install -y $INSTALL
+      return $?
+    fi
+    return 0
+}
+
 #
 # Controller
 #
 
 function main
 {
-    for step in $STEPS; do
-        STEPS_BACKTRACE+=("$step ")
+    for step in ${STEPS[@]} ; do
+        STEPS_BACKTRACE[${#STEPS_BACKTRACE[@]}]=$step
         echo -n "Executing step $step"
         execute_step $step
         if [ $? -ne 0 ]; then
             echo_failure
             echo
+            echo "=== BEGIN LOG ==="
+            cat $LAST_LOG_FILE
+            echo "=== END LOG ==="
+            echo "All logs: $LOG_FILE"
 
-            # Run back trace
-            echo "Starting back trace..."
-            for (( idx=${#STEPS_BACKTRACE[@]}-1 ; idx>=0 ; idx-- )) ; do
-                step=${STEPS_BACKTRACE[idx]}
-                echo -n "Undoing step $step"
-                undo_step $step
-                if [ $? -ne 0 ]; then
-                    echo_failure
-                else
-                    echo_success
-                fi
-                echo
-            done
+            # Backtrace?
+            echo -n 'Do you want to rewind all the changes? Type: "Yes, I do": '
+            read ANS
+            if [ "$ANS" == 'Yes, I do' ] ; then
+
+                # Run back trace
+                echo "Starting back trace..."
+                for (( idx=${#STEPS_BACKTRACE[@]}-1 ; idx>=0 ; idx-- )) ; do
+                    step=${STEPS_BACKTRACE[idx]}
+                    echo -n "Undoing step $step"
+                    undo_step $step
+                    if [ $? -ne 0 ]; then
+                        echo_failure
+                    else
+                        echo_success
+                    fi
+                    echo
+                done
+            else
+                echo 'Not undoing changes.'
+            fi
+
             exit 1
         else
             echo_success
