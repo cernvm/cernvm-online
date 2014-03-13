6182310b3cf1f89552b59f0967b7a7a4b747bba8
George Lestaris <glestaris@gmail.com>
2014-03-13 15:02:27 +0100

    Adds market Django application

---
+++ src/cvmo/market/admin.py
@@ -0,0 +1,5 @@
+from . import models
+from django.contrib import admin
+
+admin.site.register(models.MarketplaceGroup)
+admin.site.register(models.MarketplaceContextEntry)
---
+++ src/cvmo/market/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns(
+    "cvmo.market.views",
+    url(r"/revoke/(?P<context_id>[-\w]+)$", "revoke",
+        name="market_revoke"),
+    url(r"/publish/(?P<context_id>[-\w]+)$", "publish",
+        name="market_publish"),
+    url(r"/publish.do$", "publish_action",
+        name="market_publish_action"),
+    url(r"/list$", "list", name="market_list"),
+    url(r"/list.search$", "list_ajax", name="market_list_search"),
+    url(r"/vote.do$", "vote_ajax", name="market_vote")
+)
