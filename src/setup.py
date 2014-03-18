73305c4b463d4f8849abea276bfe772669728ee4
George Lestaris <glestaris@gmail.com>
2014-03-18 11:40:01 +0100

    Updates version and adds changelog

--- src/setup.py
+++ src/setup.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     setup(
         # Meta data
         name="CernVM-Online",
-        version="1.1.0",
+        version="1.2.0",
         author="CernVM",
         author_email="cernvm.administrator@cern.ch",
         description="CernVM Online portal",
