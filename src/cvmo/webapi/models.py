f1030ceb373987cad45006ad43b87aced25dac90
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2015-05-05 13:30:03 +0200

    Fixing indentation

--- src/cvmo/webapi/models.py
+++ src/cvmo/webapi/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 class WebAPIOneTimeTag(models.Model):
 
-	uuid = models.TextField(max_length=10, primary_key=True, unique=True, )
-    payload = models.TextField()
+	uuid = models.TextField(max_length=10, primary_key=True, unique=True )
+	payload = models.TextField()
 
-    def __unicode__(self):
-        return "Tag #%i" % self.id
+	def __unicode__(self):
+		return "Tag #%i" % self.id
