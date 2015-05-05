fba1c3596af0d820b4af5f30a759cb4904f143d8
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2015-05-05 13:28:24 +0200

    Fixing typos

--- src/cvmo/webapi/urls.py
+++ src/cvmo/webapi/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     "cvmo.webapi.views",
     url(r"^vmcp$",                       "vmcp",         name="webapi_vmcp"),
-    url(r"^start/(?P<tag_id>[-\w]+)$",   "webstart_run"  name="webapi_webstart_run"),
+    url(r"^start/(?P<tag_id>[-\w]+)$",   "webstart_run", name="webapi_webstart_run"),
     url(r"^start$",                      "webstart_init",name="webapi_webstart_init"),
 )
