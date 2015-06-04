b69b4ee311d1f6fadf4668588596fee89e78a914
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2015-06-04 15:33:08 +0200

    Handling disconnect state

--- src/cvmo/webapi/static/webapi/js/webapi.js
+++ src/cvmo/webapi/static/webapi/js/webapi.js
@@ -25,10 +25,17 @@
 
 	// Callback handler in case of error
 	function progress_error(message) {
-		$("#webapi-frame").removeClass("color-running");
-		$("#webapi-frame").addClass("color-error");
+		$("#webapi-frame").attr("class"," color-error");
 		$("#lbl-header").text("An error occured");
 		$("#lbl-status").text(message);
+		update_progress_value(0.0);
+	}
+
+	// Disconnected
+	function disconnected() {
+		$("#webapi-frame").attr("class"," color-disconnected");
+		$("#lbl-header").text("Disconnected from WebAPI");
+		$("#lbl-status").text("A critical error has occured which might have caused WebAPI to crash!");
 		update_progress_value(0.0);
 	}
 
@@ -46,14 +53,12 @@
 			api.addEventListener('completed', progress_completed);
 			api.addEventListener('progress', progress_updated);
 			api.addEventListener('failed', progress_error);
+			api.addEventListener('disconnected', disconnected);
 
 			// Request a session through our specified VMCP endpoint
 			api.requestSession(vmcp_url, function(session) {
 				$("#lbl-status").text("Session open");
 										
-				// Obtained from the Appendix
-				var STATE_NAMES = ['Not yet created', '', 'Powered off', 'Saved', 'Paused', 'Running'];
-
 				// Bind progress listeners on the session instance
 				session.addEventListener('started', progress_started);
 				session.addEventListener('completed', progress_completed);
@@ -62,13 +67,10 @@
 
 				// Listen for state changed events
 				session.addEventListener('stateChanged', function(newState) {
-					$("#lbl-status").text(STATE_NAMES[newState]);
-
 					// When we are running we don't need the window any more
 					if (newState == 5) {
 						window.close();
 					}
-
 				});
 
 				// We just start the session the moment we get it
