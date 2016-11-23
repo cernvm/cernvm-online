ee181fcbf327c411013123f833a5e2e290d593c5
Petr Jirout <pejirout@github.com>
2016-11-23 16:02:36 +0100

    Added cluster pairing point page
    - Added cluster pin expiration check

--- src/cvmo/rest_api/urls.py
+++ src/cvmo/rest_api/urls.py
@@ -1,18 +1,18 @@
 from django.conf.urls import url
 
-from .views import ClusterCreate, ClusterDetail, ClusterKeyValueCreate, ClusterKeyValueDetail
+import views
 from .models import CLUSTER_PIN_LENGTH, CLUSTER_PIN_ALLOWED_CHARS
 
 app_name = 'api-rest'
 urlpatterns = [
-    url(r'^clusters$', ClusterCreate.as_view(), name='cluster-create'),
+    url(r'^clusters$', views.ClusterCreate.as_view(), name='cluster-create'),
     # Match clusters/<cluster_pin>
     url(r'^clusters/(?P<pin>[%s]{%d})$' % (CLUSTER_PIN_ALLOWED_CHARS, CLUSTER_PIN_LENGTH),
-        ClusterDetail.as_view(), name='cluster-detail'),
+        views.ClusterDetail.as_view(), name='cluster-detail'),
     # Match clusters/<cluster_pin>/keys
     url(r'^clusters/(?P<pin>[%s]{%d})/keys$' % (CLUSTER_PIN_ALLOWED_CHARS, CLUSTER_PIN_LENGTH),
-        ClusterKeyValueCreate.as_view(), name='cluster-key-value-create'),
+        views.ClusterKeyValueCreate.as_view(), name='cluster-key-value-create'),
     # Match clusters/<cluster_pin>/keys/<key>
     url(r'^clusters/(?P<pin>[%s]{%d})/keys/(?P<key>[a-zA-Z0-9_-]+)$' % (CLUSTER_PIN_ALLOWED_CHARS, CLUSTER_PIN_LENGTH),
-        ClusterKeyValueDetail.as_view(), name='cluster-key-value-detail')
+        views.ClusterKeyValueDetail.as_view(), name='cluster-key-value-detail')
 ]
--- src/cvmo/rest_api/views.py
+++ src/cvmo/rest_api/views.py
@@ -1,5 +1,9 @@
+from datetime import timedelta
+
+from django.views.generic import TemplateView
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
+from django.utils import timezone
 
 from rest_framework import generics
 from rest_framework.renderers import JSONRenderer
@@ -10,8 +14,15 @@ from .serializers import ClusterSerializer, ClusterKeyValueSerializer
 from .renderers import ClusterKeyRenderer
 
 
+class ClusterPairingPage(TemplateView):
+    """ HTML page accessible from the menu, with cluster pairing point dialog. """
+
+    template_name = "rest_api/cluster_pairing.html"
+
+
 class Conflict(APIException):
     """ Exception class implementing HTTP 409 Conflict """
+
     status_code = 409
     default_detail = "Resource already exists, cannot be created again"
     default_code = 'conflict'
@@ -23,6 +34,12 @@ class ClusterCreate(generics.CreateAPIView):
     queryset = Cluster.objects.all()
     serializer_class = ClusterSerializer
 
+    def initial(self, request, *args, **kwargs):
+        """ Check the expired pins before every request. """
+
+        CheckPinExpiration()
+        super(ClusterCreate, self).initial(request, *args, **kwargs)
+
 
 class ClusterDetail(generics.RetrieveUpdateDestroyAPIView):
     """ Retrieve, update or destroy a cluster """
@@ -30,6 +47,12 @@ class ClusterDetail(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = 'pin'
     queryset = Cluster.objects.all()
     serializer_class = ClusterSerializer
+
+    def initial(self, request, *args, **kwargs):
+        """ Check the expired pins before every request. """
+
+        CheckPinExpiration()
+        super(ClusterDetail, self).initial(request, *args, **kwargs)
 
 
 class ClusterKeyValueCreate(generics.ListCreateAPIView):
@@ -51,6 +74,12 @@ class ClusterKeyValueCreate(generics.ListCreateAPIView):
         except (ObjectDoesNotExist, KeyError):
             raise Http404
 
+    def initial(self, request, *args, **kwargs):
+        """ Check the expired pins before every request. """
+
+        CheckPinExpiration()
+        super(ClusterKeyValueCreate, self).initial(request, *args, **kwargs)
+
 
 class ClusterKeyValueDetail(generics.RetrieveUpdateDestroyAPIView):
     """ Retrieve, update or destroy an element in a cluster """
@@ -63,3 +92,30 @@ class ClusterKeyValueDetail(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         # Filter objects with elements from the URL: <pin>, <key>
         return ClusterKeyValue.objects.filter(cluster__pin=self.kwargs['pin']).filter(key=self.kwargs['key'])
+
+    def initial(self, request, *args, **kwargs):
+        """ Check the expired pins before every request. """
+
+        CheckPinExpiration()
+        super(ClusterKeyValueDetail, self).initial(request, *args, **kwargs)
+
+
+def CheckPinExpiration():
+    """ Check for expired cluster pins and delete them if necessary.
+
+    We don't query the DB on each call, but at most every 2 minutes.
+    """
+
+    # Initialize our static method variable
+    if "lastCheck" not in CheckPinExpiration.__dict__:
+        CheckPinExpiration.lastCheck = timezone.now() - timedelta(weeks=56)  # well in the past
+
+    checkingInterval = timedelta(minutes=2)  # check every 2 mins for expired pins
+
+    if CheckPinExpiration.lastCheck + checkingInterval > timezone.now():
+        return  # too soon
+
+    # Update last check time
+    CheckPinExpiration.lastCheck = timezone.now()
+    # Delete objects older than 1 day
+    Cluster.objects.filter(creation_time__lt=timezone.now() - timedelta(days=1) ).delete()
